897b139f58874e92cf2412677bf7ced005cb0dfb
zzollozz <81852084+zzollozz@users.noreply.github.com>
2023-04-11 11:44:44 +0300

    Выполнил ДЗ 5

---
+++ homeWork/hw_5/hw_5_task3.java
@@ -0,0 +1,48 @@
+package homeWork.hw_5;
+
+public class hw_5_task3 {
+    public static void main(String[] args) {
+        // На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
+        // И вывести Доску. Пример вывода доски 8x8
+        // 8x8 board
+
+        int[] queens = new int[8];
+        findPosition(0, queens);
+
+        // Вывод доски в консоль
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (queens[row] == col) {
+                    System.out.print("X");
+                } else {
+                    System.out.print("o");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Функция для поиска расстановки ферзей
+    private static boolean findPosition(int row, int[] queens) {
+        if (row == 8) { // Если удалось расставить всех 8 ферзей, вернуть true
+            return true;
+        }
+        for (int col = 0; col < 8; col++) {
+            boolean isSafe = true;
+            for (int i = 0; i < row; i++) {
+                // Проверка на наличие конфликта по вертикали, диагонали и антидиагонали
+                if (queens[i] == col || queens[i] == col - row + i || queens[i] == col + row - i) {
+                    isSafe = false;
+                    break;
+                }
+            }
+            if (isSafe) {
+                queens[row] = col; // Сохранить расстановку ферзя в массив
+                if (findPosition(row + 1, queens)) { // Рекурсивный вызов для следующего ряда
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
